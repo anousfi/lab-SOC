bea251844f4b9b4f945feb4f40fdda83157ee41a
unknown <anousfi@gmail.com>
2026-06-26 16:08:41 +0100

    Update - changed elasticsearch restarting

--- terraform/main.tf
+++ terraform/main.tf
@@ -243,7 +243,12 @@ resource "aws_security_group" "elastic_sg"{
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"]
   }
-
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["public address"]
+  }
 
   egress {
     from_port   = 0
@@ -277,6 +282,12 @@ resource "aws_security_group" "kibana_sg" {
     cidr_blocks = ["10.0.0.0/16"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["public address"]
+  }
 
   egress {
     from_port   = 0
